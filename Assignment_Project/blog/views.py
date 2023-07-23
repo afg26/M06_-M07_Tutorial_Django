299464fef046069bdc389bc2fcb5292205e31be2
afg26 <a.f.ghaznawi1991@gmail.com>
2023-07-22 22:07:53 -0400

    final

--- Assignment_Project/blog/views.py
+++ Assignment_Project/blog/views.py
@@ -13,8 +13,11 @@ from django.contrib.auth.decorators import login_required
 
 
 #urls are the gates to open the pages but what we want to open is listed here and every section and every function is very important
-#
 
+
+
+
+#these are the two main webpages which is ran on the launch.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -24,6 +27,9 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+#what login_required means is that if you are not login you can not make any changes or add anything, once you have an account then you can make changes adn add new posts.
+#down here multiple functions have been made and doing the task like edit, add new, remove and give comments.
+#remember that you can add comments and but the user has approve those comments then your comments will be visible.
 @login_required
 def post_new(request):
     if request.method == "POST":
